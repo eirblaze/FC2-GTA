a49990e2bc80305a5d901f16098655ee3ed88035
eirblaze <eirblaze@live.jp>
2018-12-24 01:02:08 +0900

    CDN環境構築完了

---
+++ html/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Rawgit終了にあたり、とりいそぎjsDelivrでアセットを配信する
+ * @see https://qiita.com/pokkur/items/40ded290fc5122bfe238
+ *
+ * CDN設定例 : https://cdn.jsdelivr.net/gh/pokkur/proxemics@latest/dist/proxemics.min.js
+ *
+ * https://github.com/eirblaze/FC2-GTA/blob/master/html/js/script.min.js
+ * @cdn https://cdn.jsdelivr.net/gh/eirblaze/FC2-GTA@latest/html/dist/script.min.js
+ */
+console.log('hello');
+console.log('world');
